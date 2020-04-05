f3413ef6076138870a035c13da1fd499e33d82a3
Pablo Bonilla <pbonilla29@hotmail.com>
2020-04-05 13:58:52 -0600

    Add creation of vertex visually

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 const svg = document.querySelector('#svg');
 const paths = Array.from(svg.getElementsByTagName("path"));
+const environment = document.querySelector('#environment');
+let selectedCountries = [];
+
 paths.forEach(e => {
     e.removeAttribute('style');
     e.classList.add('country');
     e.addEventListener('click', (x) => {
-        console.log(x.target.getAttribute('data-id'));
+        console.log('x: ', x.x, 'y: ', x.y);
+        let name = x.target.getAttribute('data-id');
+        createVertex(x.x, x.y, name);
     });
 });
+
+
+function createVertex(x, y, name) {
+    if (!selectedCountries.includes(name)) {
+        let div = document.createElement('div');
+        div.classList.add('vertexForm');
+        div.style.position = 'absolute';
+        div.style.top = y - 20;
+        div.style.left = x - 20;
+        div.innerText = name;
+        environment.appendChild(div);
+        selectedCountries.push(name);
+    }
+}
