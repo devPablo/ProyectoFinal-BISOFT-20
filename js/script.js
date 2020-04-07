3cb3023eedb1eb914955bd4cacbb7eb342685d7f
Pablo Bonilla <pbonilla29@hotmail.com>
2020-04-07 14:48:00 -0600

    Add UI edge form

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
 const svg = document.querySelector('#svg');
 let paths = Array.from(svg.getElementsByTagName("path"));
 const environment = document.querySelector('#environment');
+const edgeForm = document.querySelector('#edgeForm');
 let selectedCountries = [];
+let selectedVertex = [];
+let controller = new Controller(25);
 
 updatePathsListener();
 
-
-function createVertex(x, y, name) {
-    if (!selectedCountries.includes(name)) {
+function createVertex(x, y, ele) {
+    let repeated = false;
+    selectedCountries.forEach(e => {
+        if (e.innerText == ele.getAttribute('data-id')) {
+            repeated = true;
+        }
+    });
+    if (!repeated) {
         let div = document.createElement('div');
         div.classList.add('vertexForm');
         div.style.top = y-20;
         div.style.left = x-20;
-        div.innerText = name;
+        div.innerText = ele.getAttribute('data-id');
 
-        div.setAttribute('x', x-20);
-        div.setAttribute('y', y-20);
+        div.setAttribute('x', x-10);
+        div.setAttribute('y', y-10);
         div.setAttribute('selected', false);
 
         environment.appendChild(div);
         selectedCountries.push(div);
 
+        controller.addNewVertice(ele.getAttribute('data-id'));
+
         div.addEventListener('click', (e) => {
             e.target.setAttribute('selected', true);
+            selectedVertex.push(e.target);
             playSound();
             checkCreateEdge();
         });
@@ -30,32 +41,27 @@ function createVertex(x, y, name) {
 }
 
 function checkCreateEdge() {
-    let vertex = [];
-    for (let i = 0; i < selectedCountries.length; i++) {
-        if (selectedCountries[i].attributes.selected.value == 'true') {
-            vertex.push(selectedCountries[i]);
-        }
-    }
-    console.log(vertex);
-    if (vertex.length == 2) {
-        selectedCountries.forEach(e => {
-            e.setAttribute('selected', false);
-        })
-        createEdge(vertex[0], vertex[1]);
+    if (selectedVertex.length == 2) {
+        console.log(selectedVertex);
+        createEdge(selectedVertex[0], selectedVertex[1], true);
+        selectedVertex = [];
     }
 }
 
-function createEdge(source, destination) {
-    console.log(source, destination)
-    let line = `<line x1="${source.getAttribute('x')}" y1="${source.getAttribute('y')}" x2="${destination.getAttribute('x')}" y2="${destination.getAttribute('y')}" style="stroke:rgb(255,0,0);stroke-width:2" />`
+function createEdge(source, destination, bidirectional) {
+    let line;
+    if (bidirectional) {
+        line = `<line class="edge bidirectional" x1="${parseInt(source.getAttribute('x'))}" y1="${parseInt(source.getAttribute('y'))}" x2="${parseInt(destination.getAttribute('x'))}" y2="${parseInt(destination.getAttribute('y'))}" />`
+    } else {
+        line = `<line class="edge" x1="${parseInt(source.getAttribute('x'))}" y1="${parseInt(source.getAttribute('y'))}" x2="${parseInt(destination.getAttribute('x'))}" y2="${parseInt(destination.getAttribute('y'))}" />`
+    }
     svg.innerHTML += line;
     updatePathsListener();
+
+    controller.addNewEdge(source.innerText, destination.innerText, 100, true);
 }
 
-function playSound() {
-    let audio = new Audio('./resources/pop.flac');
-    audio.play();
-}
+
 
 function updatePathsListener() {
     paths = Array.from(svg.getElementsByTagName("path"));
@@ -64,8 +70,23 @@ function updatePathsListener() {
         e.classList.add('country');
         e.addEventListener('click', (x) => {
             console.log('x: ', x.x, 'y: ', x.y);
-            let name = x.target.getAttribute('data-id');
-            createVertex(x.x, x.y, name);
+            let ele = x.target;
+            createVertex(x.x, x.y, ele);
         });
     });
+}
+
+function playSound() {
+    let audio = new Audio('./resources/pop.flac');
+    audio.play();
+}
+
+// Edge Form
+
+function showEdgeForm() {
+    edgeForm.style.display = 'inline-block';
+}
+
+function closeEdgeForm() {
+    edgeForm.style.display = 'none';
 }
