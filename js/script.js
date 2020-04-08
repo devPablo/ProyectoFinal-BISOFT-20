26f104ae91d872e8a809fdf78ee8011e37b2e59a
Pablo Bonilla <pbonilla29@hotmail.com>
2020-04-07 20:04:55 -0600

    Update bidirectional assignment for edges with cost inside SVG line

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@ const svg = document.querySelector('#svg');
 let paths = Array.from(svg.getElementsByTagName("path"));
 const environment = document.querySelector('#environment');
 const edgeForm = document.querySelector('#edgeForm');
+const btnCreateEdge = document.querySelector('#btnCreateEdge');
+btnCreateEdge.addEventListener('click', () => {
+    executeBtnCreateEdge();
+});
+const inputCost = document.querySelector('#inputCost');
+const directions = document.querySelectorAll('.direction');
+for (let i = 0; i < 2; i++) {
+    directions[i].addEventListener('click', (e) => {
+        e = e.target;
+        resetDirectionSelected();
+        e.classList.add('directionSelected');
+        e.setAttribute('selected', true);
+    });
+}
+
 let selectedCountries = [];
 let selectedVertex = [];
 let controller = new Controller(25);
@@ -22,9 +37,10 @@ function createVertex(x, y, ele) {
         div.style.left = x-20;
         div.innerText = ele.getAttribute('data-id');
 
-        div.setAttribute('x', x-10);
-        div.setAttribute('y', y-10);
+        div.setAttribute('x', x);
+        div.setAttribute('y', y);
         div.setAttribute('selected', false);
+        div.setAttribute('connections', '');
 
         environment.appendChild(div);
         selectedCountries.push(div);
@@ -42,23 +58,24 @@ function createVertex(x, y, ele) {
 
 function checkCreateEdge() {
     if (selectedVertex.length == 2) {
-        console.log(selectedVertex);
-        createEdge(selectedVertex[0], selectedVertex[1], true);
-        selectedVertex = [];
+        showEdgeForm();
     }
 }
 
-function createEdge(source, destination, bidirectional) {
+function createEdge(source, destination, cost, bidirectional) {
     let line;
-    if (bidirectional) {
-        line = `<line class="edge bidirectional" x1="${parseInt(source.getAttribute('x'))}" y1="${parseInt(source.getAttribute('y'))}" x2="${parseInt(destination.getAttribute('x'))}" y2="${parseInt(destination.getAttribute('y'))}" />`
-    } else {
-        line = `<line class="edge" x1="${parseInt(source.getAttribute('x'))}" y1="${parseInt(source.getAttribute('y'))}" x2="${parseInt(destination.getAttribute('x'))}" y2="${parseInt(destination.getAttribute('y'))}" />`
+    let connections = source.innerText + ',' + destination.innerText + ';';
+    if (bidirectional == true) {
+        console.log('in');
+        connections = source.innerText + ',' + destination.innerText + ';' + '' + destination.innerText + ',' + source.innerText + ';';
+        line = `<line class="edge bidirectional" x1="${parseInt(source.getAttribute('x'))}" y1="${parseInt(source.getAttribute('y'))}" x2="${parseInt(destination.getAttribute('x'))}" y2="${parseInt(destination.getAttribute('y'))}" cost="${cost}" connections="${connections}" />`
+    } else {        
+        line = `<line class="edge" x1="${parseInt(source.getAttribute('x'))}" y1="${parseInt(source.getAttribute('y'))}" x2="${parseInt(destination.getAttribute('x'))}" y2="${parseInt(destination.getAttribute('y'))}" cost="${cost}" connections="${connections}" />`
     }
     svg.innerHTML += line;
     updatePathsListener();
 
-    controller.addNewEdge(source.innerText, destination.innerText, 100, true);
+    controller.addNewEdge(source.innerText, destination.innerText, cost, bidirectional);
 }
 
 
@@ -69,7 +86,6 @@ function updatePathsListener() {
         e.removeAttribute('style');
         e.classList.add('country');
         e.addEventListener('click', (x) => {
-            console.log('x: ', x.x, 'y: ', x.y);
             let ele = x.target;
             createVertex(x.x, x.y, ele);
         });
@@ -85,8 +101,26 @@ function playSound() {
 
 function showEdgeForm() {
     edgeForm.style.display = 'inline-block';
+    inputCost.focus();
 }
 
 function closeEdgeForm() {
     edgeForm.style.display = 'none';
+}
+
+function executeBtnCreateEdge() {
+    let cost = inputCost.value;
+    let bidirectional = (document.querySelectorAll('.direction')[1].childNodes[0].getAttribute('selected'));
+    createEdge(selectedVertex[0], selectedVertex[1], cost, bidirectional);
+    selectedVertex = [];
+    inputCost.value = '';
+    resetDirectionSelected();
+    closeEdgeForm();
+}
+
+function resetDirectionSelected() {
+    directions[0].childNodes[0].classList.remove('directionSelected');
+    directions[0].childNodes[0].setAttribute('selected', false);
+    directions[1].childNodes[0].classList.remove('directionSelected');
+    directions[1].childNodes[0].setAttribute('selected', false);
 }
